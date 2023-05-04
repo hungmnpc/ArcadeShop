182a231c756fba35361496dc9c09996842a88d24
Đinh Quốc Hưng <hungkaiken2@gmail.com>
2023-05-04 09:21:40 +0700

    update

---
+++ BE/src/main/java/com/monopoco/arcade/trigger/NewTrigger.java
@@ -0,0 +1,8 @@
+package com.monopoco.arcade.trigger;
+/**
+ * @author: hungdinh
+ * Date created: 11/04/2023
+ */
+public class NewTrigger {
+
+}
--- FE/src/components/ProductDetailAdmin/index.js
+++ FE/src/components/ProductDetailAdmin/index.js
@@ -1,5 +1,5 @@
 import { faClone, faTrashCan } from "@fortawesome/free-regular-svg-icons";
-import {faEllipsis} from "@fortawesome/free-solid-svg-icons";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames/bind";
 import { createContext, useContext, useEffect, useReducer, useRef, useState } from "react";
@@ -41,7 +41,7 @@ const productActionsType = Object.freeze({
     SAVE_PRODUCT: 'SAVE_PRODUCT',
     INIT_PRODUCT: 'INIT_PRODUCT',
     CHANGE_MAIN_IMAGE_ID: 'CHANGE_MAIN_IMAGE_ID',
-    CHANGE_IMAGES_ID: 'CHANGE_IMAGES_ID' 
+    CHANGE_IMAGES_ID: 'CHANGE_IMAGES_ID'
 });
 
 export const createNewProduct = () => ({
@@ -238,7 +238,7 @@ const productReducer = (state = initProductState, action) => {
 
 
 export const ProductContext = createContext();
-function ProductDetailAdmin({ isScrollOver, id, isCopy=false, ...prop }) {
+function ProductDetailAdmin({ isScrollOver, id, isCopy = false, ...prop }) {
 
 
 
@@ -255,83 +255,83 @@ function ProductDetailAdmin({ isScrollOver, id, isCopy=false, ...prop }) {
         let newProduct = {};
         if (id) {
             get(`/api/v1/admin/products/${id}`)
-            .then(response => {
-                console.log(response)
-                newProduct = {
-                    ...productState,
-                    name: response.data.name,
-                    ribbon: response.data.ribbon,
-                    description: response.data.description,
-                    price: response.data.price,
-                    discountMode: response.data.discountModeName,
-                    discountValue: response.data.discountValue,
-                    imagesId: 
-                      response.data.imageSet.map(image => image.id)
-                    ,
-                    additionalInfo: response.data.additionalInfo,
-                    categories: response.data.categoriesName,
-                    visible: response.data.visible,
-                    inventoryStatus: response.data.inventoryStatus,
-                    sku: response.data.sku
-                }
-                dispatch(initProduct(newProduct));
-            })
+                .then(response => {
+                    console.log(response)
+                    newProduct = {
+                        ...productState,
+                        name: response.data.name,
+                        ribbon: response.data.ribbon,
+                        description: response.data.description,
+                        price: response.data.price,
+                        discountMode: response.data.discountModeName,
+                        discountValue: response.data.discountValue,
+                        imagesId:
+                            response.data.imageSet.map(image => image.id)
+                        ,
+                        additionalInfo: response.data.additionalInfo,
+                        categories: response.data.categoriesName,
+                        visible: response.data.visible,
+                        inventoryStatus: response.data.inventoryStatus,
+                        sku: response.data.sku
+                    }
+                    dispatch(initProduct(newProduct));
+                })
         } else if (prop.productCopyId) {
             get(`/api/v1/admin/products/${prop.productCopyId}`)
-            .then(response => {
-                newProduct = {
-                    ...productState,
-                    name: response.data.name,
-                    ribbon: response.data.ribbon,
-                    description: response.data.description,
-                    price: response.data.price,
-                    discountMode: response.data.discountModeName,
-                    discountValue: response.data.discountValue,
-                    imagesId: [],
-                    additionalInfo: response.data.additionalInfo,
-                    categories: response.data.categoriesName,
-                    visible: response.data.visible,
-                    inventoryStatus: response.data.inventoryStatus,
-                    sku: response.data.sku
-                }
-                dispatch(initProduct(newProduct));
-            })
+                .then(response => {
+                    newProduct = {
+                        ...productState,
+                        name: response.data.name,
+                        ribbon: response.data.ribbon,
+                        description: response.data.description,
+                        price: response.data.price,
+                        discountMode: response.data.discountModeName,
+                        discountValue: response.data.discountValue,
+                        imagesId: [],
+                        additionalInfo: response.data.additionalInfo,
+                        categories: response.data.categoriesName,
+                        visible: response.data.visible,
+                        inventoryStatus: response.data.inventoryStatus,
+                        sku: response.data.sku
+                    }
+                    dispatch(initProduct(newProduct));
+                })
         } else {
             newProduct = {
                 name: "",
-                    ribbon: "",
-                    description: "",
-                    categories: [
-                    ],
-                    visible: true,
-                    imagesId: [
-                      
-                    ],
-                    mainImageId: undefined,
-                    additionalInfo: {
-                    },
-                    price: null,
-                    discountMode: "PERCENT",
-                    discountValue: 0,
-                    inventoryStatus: "In stock",
-                    sku: ""
+                ribbon: "",
+                description: "",
+                categories: [
+                ],
+                visible: true,
+                imagesId: [
+
+                ],
+                mainImageId: undefined,
+                additionalInfo: {
+                },
+                price: null,
+                discountMode: "PERCENT",
+                discountValue: 0,
+                inventoryStatus: "In stock",
+                sku: ""
             }
             dispatch(initProduct(newProduct));
         }
-        
-    },[id, prop.productCopyId])
+
+    }, [id, prop.productCopyId])
 
     console.log(productState)
 
     const handleSave = () => {
-        console.log(productState)
+        
         if (!id) {
             const data = {
                 ...productState,
                 discountValue: parseFloat(productState.discountValue),
                 price: parseFloat(productState.price)
             }
-                    
+
             toast.promise(addNewProduct(data), {
                 loading: 'Creating.....',
                 success: 'Successfull!',
@@ -351,7 +351,7 @@ function ProductDetailAdmin({ isScrollOver, id, isCopy=false, ...prop }) {
                 success: 'Successfull!',
                 error: "Error. Try again!"
             }).then(response => {
-    
+
                 return navigate("/dashboard")
             }).catch(error => console.log(error))
         }
@@ -378,76 +378,76 @@ function ProductDetailAdmin({ isScrollOver, id, isCopy=false, ...prop }) {
     }
 
     return (
-    !_.isEmpty(productState) && <ProductContext.Provider value={[productState, dispatch]}>
-        
-        <div className={cx('wrapper')}>
-        <div className={cx('header-color')} />
-        <div className={cx('content')}>
-            <div className={cx('header', [isScrollOver ? 'minimize' : ''])}>
-                <div className={cx("breadcrumbs")}>
-                    <span className={cx('breadcrumb-item')}>
-                        <Link to={`/dashboard${routes.dashboardStoreProducts.products}`}>Products</Link>
-                    </span>
-                    <span className={cx('breadcrumb-item')}>
-                        {productState.name !== "" ? productState.name : UNTITLED_PRODUCT}
-                    </span>
-                </div>
-                <div className={cx('header-row')}>
-                    <div className={cx('header-title', [isFocus ? 'is-focus' : ''])}>
-                        <label onClick={() => setIsFocus(true)} htmlFor="title" className={cx('label-title')}>{productState.name !== "" ? productState.name : UNTITLED_PRODUCT}</label>
-                        <input className={cx('input-title')} onFocus={() => {
-                            setIsFocus(true)
-                        }} onBlur={() => {
-                            setIsFocus(false)
-                        }} name="title" id='title' value={productState.name} placeholder={UNTITLED_PRODUCT} onChange={(event) => {
-                            dispatch(changeNameProduct(event.target.value))
-                        }} />
-                    </div>
-                    <div className={cx('header-action')}>
-                        <ActionsDropdown />
-                        <button onClick={() => {
-                            return navigate("/dashboard")
-                        }} className={cx('btn', 'btn-cancel')}>Cancel</button>
-                        <button onClick={handleSave} className={cx('btn', 'btn-save')}>Save</button>
+        !_.isEmpty(productState) && <ProductContext.Provider value={[productState, dispatch]}>
 
+            <div className={cx('wrapper')}>
+                <div className={cx('header-color')} />
+                <div className={cx('content')}>
+                    <div className={cx('header', [isScrollOver ? 'minimize' : ''])}>
+                        <div className={cx("breadcrumbs")}>
+                            <span className={cx('breadcrumb-item')}>
+                                <Link to={`/dashboard${routes.dashboardStoreProducts.products}`}>Products</Link>
+                            </span>
+                            <span className={cx('breadcrumb-item')}>
+                                {productState.name !== "" ? productState.name : UNTITLED_PRODUCT}
+                            </span>
+                        </div>
+                        <div className={cx('header-row')}>
+                            <div className={cx('header-title', [isFocus ? 'is-focus' : ''])}>
+                                <label onClick={() => setIsFocus(true)} htmlFor="title" className={cx('label-title')}>{productState.name !== "" ? productState.name : UNTITLED_PRODUCT}</label>
+                                <input className={cx('input-title')} onFocus={() => {
+                                    setIsFocus(true)
+                                }} onBlur={() => {
+                                    setIsFocus(false)
+                                }} name="title" id='title' value={productState.name} placeholder={UNTITLED_PRODUCT} onChange={(event) => {
+                                    dispatch(changeNameProduct(event.target.value))
+                                }} />
+                            </div>
+                            <div className={cx('header-action')}>
+                                <ActionsDropdown />
+                                <button onClick={() => {
+                                    return navigate("/dashboard")
+                                }} className={cx('btn', 'btn-cancel')}>Cancel</button>
+                                <button onClick={handleSave} className={cx('btn', 'btn-save')}>Save</button>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className={cx('main-content')}>
+                        <div className={cx('main-content-column')}>
+                            <div className={cx('image-field')}>
+                                <ImageField />
+                            </div>
+                            <div className={cx('product-info')}>
+                                <ProductInfo />
+                            </div>
+                            <div className={cx('price')}>
+                                <ProductPrice />
+                            </div>
+                            <div className={cx('inventory')}>
+                                <Inventory />
+                            </div>
+
+                        </div>
+                        <div className={cx('main-content-column')}>
+                            <div className={cx('visiable-section')}>
+                                <label htmlFor="visiable" className={cx('visiable')}>
+                                    <Checkbox checked={productState.visible} handelChecked={handleVisiable} handelUnChecked={handleUnVisiable} value='visiable' name='visiable' id='visiable' />
+                                    <span>Show in online store</span>
+                                </label>
+                            </div>
+                            <div className={cx('categories-section')}>
+                                <ProductCategories isChecked={productState.categories} onChange={handleOnChaneCategories} />
+                            </div>
+                        </div>
                     </div>
 
                 </div>
+
             </div>
-            <div className={cx('main-content')}>
-                <div className={cx('main-content-column')}>
-                    <div className={cx('image-field')}>
-                        <ImageField/>
-                    </div>
-                    <div className={cx('product-info')}>
-                        <ProductInfo/>
-                    </div>
-                    <div className={cx('price')}>
-                        <ProductPrice />
-                    </div>
-                    <div className={cx('inventory')}>
-                        <Inventory/>
-                    </div>
-
-                </div>
-                <div className={cx('main-content-column')}>
-                    <div className={cx('visiable-section')}>
-                        <label htmlFor="visiable" className={cx('visiable')}>
-                            <Checkbox  checked={productState.visible} handelChecked={handleVisiable} handelUnChecked={handleUnVisiable} value='visiable' name='visiable' id='visiable' />
-                            <span>Show in online store</span>
-                        </label>
-                    </div>
-                    <div className={cx('categories-section')}>
-                        <ProductCategories isChecked={productState.categories} onChange={handleOnChaneCategories} />
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        
-    </div>
         </ProductContext.Provider>
-        );
+    );
 }
 
 
